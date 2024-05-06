adf5fc433b88303a0b65909c1e6330d9c853b538
Vivek <viveks95944@gmail.com>
2024-05-06 15:10:30 +0530

    source_code_button_added

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
-const puppeteer = require('puppeteer');
 const bodyParser = require('body-parser'); // Middleware to parse incoming requests
 
 const indexRoutes = require('./routes'); // Import the new route
@@ -20,41 +19,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.post('/generate-pdf', async (req, res) => {
-  const { html } = req.body; // Receive the HTML content from the client
-
-  if (!html) {
-    return res.status(400).json({ error: 'HTML content is required to generate PDF' });
-  }
-
-  try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-
-    // Set the HTML content to the Puppeteer page
-    await page.setContent(html, { waitUntil: 'networkidle2' });
-    const pageTitle = 'Resume';
-    
-    await page.evaluate((title) => {
-      document.title = title;
-    }, pageTitle);
-
-    // Generate PDF with A4 size and print background
-    const pdf = await page.pdf({
-      format: 'A4',
-      printBackground: true,
-    });
-
-    await browser.close();
-
-    // Send the PDF as a response
-    res.contentType('application/pdf');
-    res.send(pdf);
-  } catch (error) {
-    console.error('Error generating PDF:', error);
-    res.status(500).json({ error: 'Error generating PDF' });
-  }
-});
 
 // Use imported routes for routing
 app.use('/', indexRoutes);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,46 @@
 const express = require('express');
 const router = express.Router();
+const puppeteer = require('puppeteer');
 
 router.get('/', (req, res) => {
   res.render('home');
+});
+
+
+router.post('/generate-pdf', async (req, res) => {
+  const { html } = req.body; // Receive the HTML content from the client
+
+  if (!html) {
+    return res.status(400).json({ error: 'HTML content is required to generate PDF' });
+  }
+
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    // Set the HTML content to the Puppeteer page
+    await page.setContent(html, { waitUntil: 'networkidle2' });
+    const pageTitle = 'Resume';
+    
+    await page.evaluate((title) => {
+      document.title = title;
+    }, pageTitle);
+
+    // Generate PDF with A4 size and print background
+    const pdf = await page.pdf({
+      format: 'A4',
+      printBackground: true,
+    });
+
+    await browser.close();
+
+    // Send the PDF as a response
+    res.contentType('application/pdf');
+    res.send(pdf);
+  } catch (error) {
+    console.error('Error generating PDF:', error);
+    res.status(500).json({ error: 'Error generating PDF' });
+  }
 });
 
 module.exports = router;

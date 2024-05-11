4e9d9a647463efc95a9394d4b5c44f263c49809a
Vivek <viveks95944@gmail.com>
2024-05-11 21:29:40 +0530

    small_changes

--- public/js/handlers.js
+++ public/js/handlers.js
@@ -1,8 +1,16 @@
 $(document).ready(function () {
+   // Populate text inputs with corresponding data from the preview pane
+   $('.editor-field').each(function () {
+    var targetSelector = $(this).data('target'); // Get the target selector
+    var previewValue = $(targetSelector).text(); // Get the content from the preview pane
+    $(this).val(previewValue); // Set the text input value
+  });
+
   $('.accordion-heading').click(function () {
     // Toggle the visibility of the accordion body specified by the data-accordion-target attribute
     $($(this).data('accordion-target')).toggleClass('hidden');
   });
+
   $('#fileInput').change(function () {
     var file = this.files[0];
     if (file) {
@@ -13,13 +21,7 @@ $(document).ready(function () {
       reader.readAsDataURL(file);
     }
   });
-  // Populate text inputs with corresponding data from the preview pane
-  $('.editor-field').each(function () {
-    var targetSelector = $(this).data('target'); // Get the target selector
-    var previewValue = $(targetSelector).text(); // Get the content from the preview pane
-    $(this).val(previewValue); // Set the text input value
-  });
-
+ 
   // Event listener to keep the preview pane in sync with the editor pane
   $('.editor-pane').on('input', '.editor-field', function () {
     var targetSelector = $(this).data('target');

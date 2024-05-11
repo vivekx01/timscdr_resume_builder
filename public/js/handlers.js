2e0dcfbe4ff19898a5fa668696f476486251ac75
Vivek <viveks95944@gmail.com>
2024-05-11 11:22:48 +0530

    left_editor_behaviour_added

--- public/js/handlers.js
+++ public/js/handlers.js
@@ -1,16 +1,19 @@
-$(document).ready(function() {
-    // Populate text inputs with corresponding data from the preview pane
-    $('.editor-field').each(function() {
-      var targetSelector = $(this).data('target'); // Get the target selector
-      var previewValue = $(targetSelector).text(); // Get the content from the preview pane
-      $(this).val(previewValue); // Set the text input value
-    });
-  
-    // Event listener to keep the preview pane in sync with the editor pane
-    $('.editor-pane').on('input', '.editor-field', function() {
-      var targetSelector = $(this).data('target');
-      var newValue = $(this).val();
-      $(targetSelector).text(newValue);
-    });
+$(document).ready(function () {
+  $('.accordion-heading').click(function () {
+    // Toggle the visibility of the accordion body specified by the data-accordion-target attribute
+    $($(this).data('accordion-target')).toggleClass('hidden');
   });
-  
+  // Populate text inputs with corresponding data from the preview pane
+  $('.editor-field').each(function () {
+    var targetSelector = $(this).data('target'); // Get the target selector
+    var previewValue = $(targetSelector).text(); // Get the content from the preview pane
+    $(this).val(previewValue); // Set the text input value
+  });
+
+  // Event listener to keep the preview pane in sync with the editor pane
+  $('.editor-pane').on('input', '.editor-field', function () {
+    var targetSelector = $(this).data('target');
+    var newValue = $(this).val();
+    $(targetSelector).text(newValue);
+  });
+});

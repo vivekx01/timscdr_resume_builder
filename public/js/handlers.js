bfebf83eec84042684b806efc0ad9cab209035d1
Vivek <viveks95944@gmail.com>
2024-05-16 22:20:00 +0530

    a_tag_condition_in_handler

--- public/js/handlers.js
+++ public/js/handlers.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-   // Populate text inputs with corresponding data from the preview pane
-   $('.editor-field').each(function () {
+  // Populate text inputs with corresponding data from the preview pane
+  $('.editor-field').each(function () {
     var targetSelector = $(this).data('target'); // Get the target selector
     var previewValue = $(targetSelector).text(); // Get the content from the preview pane
     $(this).val(previewValue); // Set the text input value
@@ -21,11 +21,19 @@ $(document).ready(function () {
       reader.readAsDataURL(file);
     }
   });
- 
+
   // Event listener to keep the preview pane in sync with the editor pane
   $('.editor-pane').on('input', '.editor-field', function () {
     var targetSelector = $(this).data('target');
     var newValue = $(this).val();
-    $(targetSelector).text(newValue);
+    var $targetElement = $(targetSelector);
+
+    // Check if the target element is an <a> tag
+    if ($targetElement.is('a')) {
+      $targetElement.attr('href', newValue); // Set href attribute to the new value
+    }
+
+    $targetElement.text(newValue); // Set the text content to the new value
   });
+
 });
